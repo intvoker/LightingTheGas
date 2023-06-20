e2b8c9e7316e62a2fd36418262db50b6d6bcfa43
intvoker <intvoker@gmail.com>
2023-06-20 10:30:15 -0700

    Initial commit

---
+++ Source/LightingTheGas/LightingTheGas.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class LightingTheGas : ModuleRules
+{
+	public LightingTheGas(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
+	}
+}
---
+++ Source/LightingTheGas/LightingTheGasGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LightingTheGasGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ALightingTheGasGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ALightingTheGasGameMode();
+};
+
+
+
